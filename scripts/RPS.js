0076df4f60a6fd2a6e4c4808d89fe7e19cdc034d
GimboidGit <alan.webster@hotmail.com>
2022-06-13 16:31:52 +0100

    Update RPS.js
    Made the elements of the string Choice array UPPERCASE.
    Implemented method PlayRound; compares Player/Computer choices to see who won the round.

--- scripts/RPS.js
+++ scripts/RPS.js
@@ -1,7 +1,25 @@
-const Choices = [ "Rock", "Paper", "Scissors" ];
+const Choices = [ "ROCK", "PAPER", "SCISSORS" ];
 
 
 function ComputerPlay()
 {
     return Choices[Math.floor(Math.random() * 3)]; //Random number between 0 and 2
+}
+
+
+function PlayRound(playerChoice, computerChoice)
+{
+    playerChoice = String(playerChoice).toUpperCase();
+    computerChoice = String(computerChoice).toUpperCase();
+
+    if (playerChoice === computerChoice)
+        return "It's a Draw!";
+
+    for (let i = 0; i < Choices.length; i++)
+    {
+        if (playerChoice === Choices[i] && computerChoice === Choices[(arr.Len - i) - 1])
+            return `You Win! ${playerChoice} beats ${computerChoice}`;
+    }
+
+    return `You Lose! ${computerChoice} beats ${playerChoice}`;
 }
